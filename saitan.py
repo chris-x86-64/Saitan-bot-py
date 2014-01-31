ce3eacff892454f57d8541f13301c1ce083dd67e
Christopher Smith <chris@x86-64.info>
2014-01-31 22:02:44 +0900

    config is now working

--- saitan.py
+++ saitan.py
@@ -1,7 +1,21 @@
-import tweepy
+import sys
+import yaml
 from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option("-c", "--config", action = "store", dest = "config_file", help = "config file in yaml format.")
 (options, args) = parser.parse_args()
-print options.config_file
+
+try:
+	stream = open(options.config_file, 'r')
+except TypeError, e:
+	print e
+	print "Please specify the path to config file (--config CONFIG_FILE)"
+	sys.exit(1)
+except IOError, e:
+	print e
+	print "Specified config file not found!"
+	sys.exit(1)
+
+config = yaml.load(stream)
+print config
